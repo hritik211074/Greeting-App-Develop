514d6598b18b5bb0ad30f5bf608f045483d71a02
hritik211074 <0111al211074hritikkumarmaurya@gmail.com>
2025-02-23 13:44:58 +0530

    [Hriitk] adding the uc01 folder

---
+++ src/main/java/com/example/greetingapp/uc01_using_curlway/GreetingAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.greetingapp.uc01_using_curlway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class GreetingAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(GreetingAppApplication.class, args);
+	}
+}
